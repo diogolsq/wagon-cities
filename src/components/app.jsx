a8b871bcd6512038c4fdcd0c4c568503d5364870
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-12-31 11:29:35 -0200

    starting to see the components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import CityList from '../containers/city_list';
-import City from '../containers/city';
+import ActiveCity from '../containers/active_city';
 
 const App = () => {
   return (
